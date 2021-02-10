0a88b06f9bfc48ee411680327fd724549ae156f0
montoyamoraga <montoyamoraga@gmail.com>
2021-02-10 18:43:21 -0500

    update to fluid-music 0.9.x

--- tr-909/index.js
+++ tr-909/index.js
@@ -1,25 +1,31 @@
-const byType = require('./by-type');
-const all = require('./all');
+const { techniques, AudioFileMode } = require('fluid-music')
+const audioFiles = require('./audio-files')
 
-const nLibrary = {
-  k: byType.kick[0],
-  K: byType.kick[1],
-  h: byType.hat[0],
-  H: byType.hat[1],
-  o: byType.openHat[0],
-  s: byType.snare[0],
-  S: byType.snare[1],
-  c: byType.crash[0],
-  i: byType.hiTom[0],
-  t: byType.midTom[0],
-  T: byType.lowTom[0],
-  r: byType.ride[0],
-  R: byType.rimshot[0],
-};
+const copy = techniques.AudioFile.copy
+const params = {
+  mode: AudioFileMode.OneVoice,
+}
+
+const paramsHiHat = {
+  ...params,
+  gainDb = -9,
+}
+
+/**
+ * This curated subset of the tr-909 kit
+ */
+const tLibrary = {
+  B: copy(audioFiles.TR909all['BT0A0D7.WAV'], params),    // lower pitch
+  b: copy(audioFiles.TR909all['BT7A0D7.WAV'], params),    // higher pitch
+  h: copy(audioFiles.TR909all['HHCD4.WAV'], paramsHiHat), // closed hat
+  H: copy(audioFiles.TR909all['HHOD4.WAV'], paramsHiHat), // open hat
+  k: copy(audioFiles.TR909all['ST0T3S7.WAV'], params),    // short snare
+  K: copy(audioFiles.TR909all['ST3T7S7.WAV'], params),    // long snare
+  p: copy(audioFiles.TR909all['HANDCLP1.WAV'], params),   // clap 👏
+  '👏': copy(audioFiles.TR909all['HANDCLP1.WAV'], params) // emoji clap
+}
 
 module.exports = {
-  all,
-  byType,
-  nLibrary,
-  dir: __dirname,
-};
+  audioFiles,
+  tLibrary,
+}
